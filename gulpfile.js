ed0f6c1433d21a11152d941079be2743d4875874
Anton Burduzha <anton.burduzha@hotmail.com>
2016-10-06 16:16:00 +0300

    setup and form for adding tasks

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sassLint = require('gulp-sass-lint');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+gulp.task('watch', function () {
+    gulp.watch('src/*.html', ['html']);
+    gulp.watch('src/**/*.scss', ['sass']);
+});
+
+gulp.task('html', function () {
+    gulp.src('src/*.html')
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('sass-lint', function () {
+    return gulp.src('src/**/*.scss')
+        .pipe(sassLint({
+            options: {
+                formatter: 'stylish'
+            }
+        }))
+        .pipe(sassLint.format())
+        .pipe(sassLint.failOnError())
+});
+
+gulp.task('sass',['sass-lint'], function () {
+    gulp.src('src/styles/style.scss')
+        .pipe(sass())
+        .on('error', sass.logError)
+        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(gulp.dest('dist/css/'))
+});
+
+gulp.task('images', function () {
+    gulp.src('src/img/**/*')
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('autoprefixer', function () {
+    return gulp.src('./dist/css/style.css')
+        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('vendor', function () {
+    gulp.src('node_modules/bootstrap/dist/css/bootstrap.min.css')
+        .pipe(gulp.dest('dist/css'));
+    gulp.src('node_modules/bootstrap/dist/js/bootstrap.min.js')
+        .pipe(gulp.dest('dist/js'));
+    gulp.src('node_modules/jquery/dist/jquery.min.js')
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('build', ['html', 'sass', 'vendor', 'images', 'autoprefixer']);
